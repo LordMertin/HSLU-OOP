d0c81b786aaeebaaa805692966328c4cb50f21b1
Mertin H <mhoeger@gmx.ch>
2021-11-23 18:53:01 +0100

    sw10 dings

---
+++ src/main/java/ch/hslu/oop/SW10/Switchable.java
@@ -0,0 +1,8 @@
+package ch.hslu.oop.SW10;
+
+public interface Switchable {
+    public abstract void switchOn();
+    public abstract void switchOff();
+    public abstract boolean isSwitchedOn();
+    public abstract boolean isSwitchedOff();
+}
